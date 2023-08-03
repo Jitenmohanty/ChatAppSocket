f50a4bd3bff03a3aaf47606d844c2dde1079bf75
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-08-03 19:30:10 +0530

    Socket add

---
+++ Server/index.js
@@ -0,0 +1,20 @@
+const http = require('http')
+const express = require('express')
+const cors = require('cors')
+const {Server} = require('socket.io')
+
+const app = express(); 
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server,{
+    cors:{
+        origin:"http://localhost:3000",
+        methods:["GET","POST"]
+    }
+})
+
+server.listen(3001 , ()=>{
+    console.log("Server is Running")
+})
