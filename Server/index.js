5957104691ae311403a7c727f18b84e9951efef7
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-08-03 22:41:44 +0530

    Backend Done

--- Server/index.js
+++ Server/index.js
@@ -1,20 +1,36 @@
-const http = require('http')
-const express = require('express')
-const cors = require('cors')
-const {Server} = require('socket.io')
-
-const app = express(); 
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 app.use(cors());
 
 const server = http.createServer(app);
 
-const io = new Server(server,{
-    cors:{
-        origin:"http://localhost:3000",
-        methods:["GET","POST"]
-    }
-})
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
-server.listen(3001 , ()=>{
-    console.log("Server is Running")
-})
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
